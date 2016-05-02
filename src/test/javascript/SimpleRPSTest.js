d82283eb69d4f2938974aced6dd1beccdec4cce3
H.T. Koo <h1017760@connect.hku.hk>
2016-05-02 19:28:33 +0800

    JavaScript, QUnit setup; initiated npm package (declaring dependency on QUnitJS)

---
+++ src/test/javascript/SimpleRPSTest.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Hey on 1 May 2016.
+ *
+ * ---
+ *
+ * Unit test for Rock paper scissors
+ */
+
+/*jslint browser: true*/
+/*globals $, jQuery, QUnit*/
+
+"use strict";
+//noinspection SpellCheckingInspection
+//var Qunit = require('qunitjs');
+
+QUnit.test("ok test", function (assert) {
+    assert.ok(false, "false fails");
+});
+
+QUnit.test("test 2", function (assert) {
+    assert.ok(true, "true passes");
+});
