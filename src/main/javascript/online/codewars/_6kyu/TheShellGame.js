e6f790f8501a1b880fe5369d8bd7854f18e34705
H.T. Koo <h1017760@connect.hku.hk>
2016-08-13 15:31:37 +0800

    Codewars - solved 1 (6Kyu - TheShellGame)

---
+++ src/main/javascript/online/codewars/_6kyu/TheShellGame.js
@@ -0,0 +1,43 @@
+/**
+ * Created by Hey on 13 Aug 2016
+ */
+/*
+
+ https://www.codewars.com/kata/duplicate-encoder/train/javascript
+
+ "The Shell Game" involves three shells/cups/etc upturned on a playing surface, with a ball placed underneath one of them. The shells are then rapidly swapped round, and the game involves trying to track the swaps and, once they are complete, identifying the shell containing the ball.
+
+ This is usually a con, but you can assume this particular game is fair...
+
+ Your task is as follows. Given the shell that the ball starts under, and list of swaps, return the location of the ball at the end. All shells are indexed by the position they are in at the time.
+
+ For example, given the starting position 0 and the swap sequence [(0, 1), (1, 2), (1, 0)]:
+
+ The first swap moves the ball from 0 to 1
+ The second swap moves the ball from 1 to 2
+
+ The final swap doesn't affect the position of the ball.
+
+ So
+
+ swaps = [[0,1], [1,2], [1, 0]]
+ find_the_ball(0, swaps) == 2
+
+ There aren't necessarily only three cups in this game, but there will be at least two. You can assume all swaps are valid, and involve two distinct indices.
+
+ * */
+
+var find_the_ball = function (start, swaps) {
+    "use strict";
+    //your code here
+    swaps.forEach(function (val) {
+        if (val[0] === start) {
+            start = val[1];
+        } else if (val[1] === start) {
+            start = val[0];
+        }
+    });
+    return start;
+};
+
+module.exports = find_the_ball;
