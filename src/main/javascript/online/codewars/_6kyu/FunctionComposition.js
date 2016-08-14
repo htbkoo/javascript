e0e7799a144958a16cdb5374f6f01fb882570f55
H.T. Koo <h1017760@connect.hku.hk>
2016-08-14 12:37:36 +0800

    Codewars - solved 1 (6Kyu - FunctionComposition)

---
+++ src/main/javascript/online/codewars/_6kyu/FunctionComposition.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Hey on 14 Aug 2016
+ */
+/*
+
+ https://www.codewars.com/kata/function-composition/train/javascript
+
+ Function composition is a mathematical operation that mainly presents itself in lambda calculus and computability. It is explained well here, but this is my explanation, in simple mathematical notation:
+
+ f3 = compose( f1 f2 )
+ Is equivalent to...
+ f3(a) = f1( f2( a ) )
+
+ Your task is to create a compose function to carry out this task, which will be passed two functions or lambdas. Ruby functions will be passed, and should return, either a proc or a lambda. Remember that the resulting composed function may be passed multiple arguments!
+
+ compose(f , g)(x)
+ => f( g( x ) )
+
+ This kata is not available in haskell; that would be too easy!
+
+ * */
+
+module.exports =
+    function compose(f, g) {
+        "use strict";
+        // Compose the two functions here!
+        return function () {
+            return f(g.apply(this, arguments));
+        };
+    };
