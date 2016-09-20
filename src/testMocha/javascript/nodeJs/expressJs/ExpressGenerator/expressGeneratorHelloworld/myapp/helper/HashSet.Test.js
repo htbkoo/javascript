2faffa0cbaa7166839651c5f53cf8f4194de5203
H.T. Koo <h1017760@connect.hku.hk>
2016-09-20 22:49:41 +0800

    node.js - express.js - expressGeneratorHelloworld example - updated tests description for HashSet
    --HG--
    branch : node_js_cwbuecheler_tutorial

--- src/testMocha/javascript/nodeJs/expressJs/ExpressGenerator/expressGeneratorHelloworld/myapp/helper/HashSet.Test.js
+++ src/testMocha/javascript/nodeJs/expressJs/ExpressGenerator/expressGeneratorHelloworld/myapp/helper/HashSet.Test.js
@@ -17,21 +17,21 @@ describe("HashSet", function () {
         });
     });
     describe("Instance methods", function () {
-        it("should add item to hashSet by add() and contains() should return true", function () {
+        it("should add item to hashSet by add(item) and contains(item) should return true", function () {
             var hashSet = new HashSet();
             Test.expect(hashSet.add("str")).to.be.true;
             Test.expect(hashSet.contains("str")).to.be.true;
         });
-        it("should return false for adding duplicated item to hashSet", function () {
+        it("should return false when add(duplicatedItem) to hashSet", function () {
             var hashSet = new HashSet();
             Test.expect(hashSet.add("str")).to.be.true;
             Test.expect(hashSet.add("str")).to.be.false;
         });
-        it("should return false for contains() of non-existent item", function () {
+        it("should return false for contains(item) of non-existent item", function () {
             var hashSet = new HashSet();
             Test.expect(hashSet.contains("not exist")).to.be.false;
         });
-        it("should return false for contains() undefined or null", function () {
+        it("should return false for contains(item) undefined or null", function () {
             var hashSet = new HashSet();
             Test.expect(hashSet.contains(undefined)).to.be.false;
             Test.expect(hashSet.contains(null)).to.be.false;
@@ -44,19 +44,19 @@ describe("HashSet", function () {
             Test.expect(hashSet.add("str")).to.be.false;
             Test.expect(hashSet.size()).to.equal(1);
         });
-        it("should return true for empty() for empty hashSet and vice versa", function () {
+        it("should return true for isEmpty() for empty hashSet and vice versa", function () {
             var hashSet = new HashSet();
             Test.expect(hashSet.isEmpty()).to.be.true;
             hashSet.add("str");
             Test.expect(hashSet.isEmpty()).to.be.false;
         });
-        it("should be able to remove from hashSet", function () {
+        it("should be able to remove(item) from hashSet", function () {
             var hashSet = new HashSet();
             Test.expect(hashSet.remove("str")).to.be.false;
             hashSet.add("str");
             Test.expect(hashSet.remove("str")).to.be.true;
         });
-        it("should be able to clear hashSet", function () {
+        it("should be able to clear() hashSet", function () {
             var hashSet = new HashSet();
             Test.expect(hashSet.size()).to.equal(0);
             hashSet.clear();
@@ -72,19 +72,19 @@ describe("HashSet", function () {
         });
     });
     describe("Static methods", function () {
-        it("should be able to create from Array and have same items", function () {
+        it("should be able to createFromArray(arr) and have same items", function () {
             var array = ["1", "2", "3"];
             var hashSet = HashSet.createFromArray(array);
             array.forEach(function (value) {
                 Test.expect(hashSet.contains(value)).to.be.true;
             });
         });
-        it("should be able to create from Array but remove duplicated items", function () {
+        it("should be able to createFromArray(arr) but remove duplicated items", function () {
             var array = ["1", "2", "1"];
             var hashSet = HashSet.createFromArray(array);
             Test.expect(hashSet.size()).to.equal(2);
         });
-        it("should be able to create from Object and have same items", function () {
+        it("should be able to createFromObject(obj) and have same items", function () {
             var obj = {"1": "", "2": "", "3": ""};
             var hashSet = HashSet.createFromObject(obj);
             Object.keys(obj).forEach(function (value) {
