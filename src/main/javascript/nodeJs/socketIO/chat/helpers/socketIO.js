312917756ff4202dc4b6b37668ae441b0c1d71c5
H.T. Koo <h1017760@connect.hku.hk>
2016-10-06 08:32:20 +0800

    socket.IO - separated socket.IO part from www
    --HG--
    branch : socket.IO

--- src/main/javascript/nodeJs/socketIO/chat/helpers/socketIO.js
+++ src/main/javascript/nodeJs/socketIO/chat/helpers/socketIO.js
@@ -1,3 +1,24 @@
 /**
- * Created by Hey on 3 Oct 2016.
+ * Created by Hey on 3 Oct 2016
  */
+
+var createIO = function (server) {
+    'use strict';
+    // initialize socket.io
+    var io = require('socket.io')(server);
+
+    var onConnection = function (socket) {
+        console.log('a user connected');
+        socket.on('disconnect', function () {
+            console.log('user disconnected');
+        });
+        socket.on('chat message', function (msg) {
+            console.log('message: ' + msg);
+            io.emit("chat message", msg);
+        });
+    };
+    io.on('connection', onConnection);
+    return io;
+};
+
+module.exports = createIO;
