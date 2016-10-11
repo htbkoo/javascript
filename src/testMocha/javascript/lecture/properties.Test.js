67d91b72587f48f80d536b606af2788ed0f57a07
H.T. Koo <h1017760@connect.hku.hk>
2016-10-11 19:27:51 +0800

    Lecture - properties in js - playing with getters/setters (done as of 11-Oct-2016)

--- src/testMocha/javascript/lecture/properties.Test.js
+++ src/testMocha/javascript/lecture/properties.Test.js
@@ -24,16 +24,30 @@ describe("properties", function () {
     });
     describe("setters", function () {
         it("should be able to export setters", function () {
-            Test.expect(properties.a).to.equal(1);
-            Test.expect(properties.a).to.equal(1);
-
+            properties.z = 99;
+            Test.expect(properties.a).to.equal(99);
+            properties.z = 2;
+            Test.expect(properties.a).to.equal(2);
         });
-        it("should be able to export getters for function", function () {
+    });
+    describe("scope", function () {
+        it("should be able to export closure - variable being referred to still get updated", function () {
+            properties.z = 99;
             Test.expect(properties.y).to.be.a('function');
-            Test.expect(properties.y()).to.equal(1);
+            Test.expect(properties.y()).to.equal(99);
+            properties.z = 3;
+            Test.expect(properties.y).to.be.a('function');
+            Test.expect(properties.y()).to.equal(3);
         });
-        it("should be able to export getters as privileged method", function () {
-            Test.expect(properties.a).to.equal(1);
+        it("should be able to export variable with local scope", function () {
+            Test.expect(properties.aWithoutClosure).to.equal(1);
+            properties.z = 4;
+            Test.expect(properties.aWithoutClosure).to.equal(1);
+        });
+        it("should be able to export variable with local scope in easier way", function () {
+            Test.expect(properties.aWithoutClosureEasier).to.equal(1);
+            properties.z = 5;
+            Test.expect(properties.aWithoutClosureEasier).to.equal(1);
         });
     });
 });
