53eb4aaedcd0c344936173fbd55278b7be00f0d7
H.T. Koo <h1017760@connect.hku.hk>
2016-11-11 00:14:11 +0800

    node.js - express.js - expressGeneratorHelloworld example - cleaned up Karma settings
    --HG--
    branch : node_js-quote

--- src/main/javascript/nodeJs/expressJs/ExpressGenerator/expressGeneratorHelloworld/myapp/test-main.js
+++ src/main/javascript/nodeJs/expressJs/ExpressGenerator/expressGeneratorHelloworld/myapp/test-main.js
@@ -1,5 +1,4 @@
 var allTestFiles = [];
-// var TEST_REGEXP = /(spec|test)\.js$/i
 var TEST_REGEXP = /\.(Test)\.js$/i;
 var EXCLUDED_REGEXP = /(node_modules\/)/i;
 
@@ -7,16 +6,12 @@ var EXCLUDED_REGEXP = /(node_modules\/)/i;
 Object.keys(window.__karma__.files).forEach(function (file) { // jshint ignore:line
     'use strict';
     if (!EXCLUDED_REGEXP.test(file)) {
-        // console.log(file);
         if (TEST_REGEXP.test(file)) {
-            // console.log("matched: " + file);
-
             // Normalize paths to RequireJS module names.
             // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
             // then do not normalize the paths
             var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '');
 
-            // console.log("normalized: " + normalizedTestModule);
             allTestFiles.push(normalizedTestModule);
         }
     }
