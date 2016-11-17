9cae6a461a5aa29504f14f557869114ffdb357b2
H.T. Koo <h1017760@connect.hku.hk>
2016-11-17 11:12:10 +0800

    Codewars - solved 1 (7kyu - CodingMeetup_5_PrepareTheCountOfLanguages)

---
+++ src/main/javascript/online/codewars/higherOrderFunctionsSeries/CodingMeetup_5_PrepareTheCountOfLanguages.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Hey on 17 Nov 2016
+ */
+
+/*
+
+ https://www.codewars.com/kata/coding-meetup-number-5-higher-order-functions-series-prepare-the-count-of-languages/train/javascript
+
+ You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+
+ Your task is to return one of the following strings:
+
+ < firstName here >, < country here > of the first Python developer who has signed up; or
+ There will be no Python developers if no Python developer has signed up.
+
+ For example, given the following input array:
+
+ var list1 = [
+ { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+ { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+ { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+ ];
+
+ your function should return Victoria, Puerto Rico.
+
+ Notes:
+
+ The input array will always be valid and formatted as in the example above.
+
+ * */
+module.exports =
+    function countLanguages(list) {
+        "use strict";
+        // thank you for checking out the Coding Meetup kata :)
+        var counts = {};
+        list.forEach(function (d) {
+            var language = d.language;
+            if (language in counts) {
+                counts[language]++;
+            } else {
+                counts[language] = 1;
+            }
+        });
+        return counts;
+    };
