c980acd22d15268d9d791bdb69959c1d7ed2e87e
H.T. Koo <h1017760@connect.hku.hk>
2016-11-28 19:33:57 +0800

    node.js - incorporate webpack into karma
    --HG--
    branch : node_js-quote

--- src/main/javascript/nodeJs/expressJs/ExpressGenerator/expressGeneratorHelloworld/myapp/karma.conf.js
+++ src/main/javascript/nodeJs/expressJs/ExpressGenerator/expressGeneratorHelloworld/myapp/karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration
 // Generated on Thu Nov 03 2016 23:37:23 GMT+0800 (China Standard Time)
 'use strict';
+var webpackConfig = require('./webpack.config');
+webpackConfig.entry = {};
 
 module.exports = function (config) {
     config.set({
@@ -8,36 +10,47 @@ module.exports = function (config) {
         // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
 
-
         // frameworks to use
         // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['mocha', 'requirejs', 'chai'],
 
-
         // list of files / patterns to load in the browser
         files: [
-            {pattern: 'public/**/*.js', included: false},
-            {pattern: 'test/**/*.Test.js', included: false},
+            // {pattern: 'dev_public/**/[!test]*/*.js', included: false},
+            {pattern: 'dev_public/**/*.js', included: false},
+            // {pattern: 'test/**/*.Test.js', included: false},
 
             {pattern: 'node_modules/chai/**/*.js', included: false},
 
             'test-main.js'
         ],
 
-
         // list of files to exclude
         exclude: ['app.js'],
 
-
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            // add webpack as preprocessor
+            // 'dev_public/**/*.js': ['coverage'],
+            'test/**/*.Test.js': ['coverage'],
 
+            // "./dev_public/js/quotes.js": ['webpack'],
+            "./dev_public/js/test_unit/quotes.Test.js": ['webpack']
+        },
+
+        webpack: webpackConfig,
+
+        webpackMiddleware: {
+            // webpack-dev-middleware configuration
+            // i. e.
+            stats: 'errors-only'
+        },
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'],
+        reporters: ['progress', 'coverage'],
 
 
         // web server port
