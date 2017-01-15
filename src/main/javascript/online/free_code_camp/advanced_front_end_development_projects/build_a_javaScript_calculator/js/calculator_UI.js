11a6254a208f5d21b9c8b87b58fa5feae0448a3e
H.T. Koo <h1017760@connect.hku.hk>
2017-01-15 16:12:44 +0800

    FreeCodeCamp - WIP 1 (Build a JavaScript Calculator) - UI -> adding keyboard control

--- src/main/javascript/online/free_code_camp/advanced_front_end_development_projects/build_a_javaScript_calculator/js/calculator_UI.js
+++ src/main/javascript/online/free_code_camp/advanced_front_end_development_projects/build_a_javaScript_calculator/js/calculator_UI.js
@@ -52,7 +52,44 @@ var Calculator = Calculator || ((typeof require !== "undefined") ? require("./Ca
                 });
             }
 
+            function setUpKeysControl() {
+                $("body").keyup(function (event) {
+                    var KEY_CODE_MAP = {
+                        "8": "BackSpace",
+                        "190": ".",
+                        "61": "=",
+                        "107": "+",
+                        "109": "-",
+                        "106": "*",
+                        "111": "/"
+                    };
+                    // {"48":"0, "49":"1", ......, "57":"9"}
+                    new Array(10).fill(0).forEach(function (_, index) {
+                        var NUMPAD_ZERO_KEY_CODE = 96;
+                        KEY_CODE_MAP[index + "0".charCodeAt(0)] = index;
+                        KEY_CODE_MAP[index + NUMPAD_ZERO_KEY_CODE] = index;
+                    });
+                    [
+                        "A",
+                        "C"
+                    ].forEach(function (key) {
+                        KEY_CODE_MAP[key.charCodeAt(0)] = key;
+                    });
+
+                    var EFFECTIVE_KEYS = {
+
+                    };
+
+                    var keyCode = event.which;
+                    if (keyCode in KEY_CODE_MAP) {
+                        event.preventDefault();
+                        EFFECTIVE_KEYS[KEY_CODE_MAP[keyCode]]();
+                    }
+                });
+            }
+
             addButtons();
+            setUpKeysControl();
             updateUI();
         }());
     }(Calculator)
