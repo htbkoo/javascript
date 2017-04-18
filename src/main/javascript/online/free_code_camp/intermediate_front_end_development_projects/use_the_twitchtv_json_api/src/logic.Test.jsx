65d8470bc455d8b69840519103f9e1b23348925a
H.T. Koo <h1017760@connect.hku.hk>
2017-04-18 20:05:30 +0800

    FreeCodeCamp - WIP 1 (Use the Twitchtv JSON API): refactored logic.Test.jsx so parameterized test would be possible

--- src/main/javascript/online/free_code_camp/intermediate_front_end_development_projects/use_the_twitchtv_json_api/src/logic.Test.jsx
+++ src/main/javascript/online/free_code_camp/intermediate_front_end_development_projects/use_the_twitchtv_json_api/src/logic.Test.jsx
@@ -1,3 +1,4 @@
+import format from "string-format";
 import {expect} from "chai";
 import sinon from "sinon";
 import sinonTest from "sinon-test";
@@ -22,32 +23,39 @@ describe("TwitchTV - FreeCodeCamp - logic test", function () {
         describe("TwitchTV (logic part)", function () {
 
             describe('getJsonFromTwitchTV', function () {
-                it('should, with request for 1 valid streamer id, get response from TwitchTV API', sinon.test(function (done) {
-                    setupJsdomAndAssertWith((err, window, $) => {
-                        //    Given
-                        let a_valid_response = [a_valid_response_obj];
-                        let $_getJSON = this.stub($, "getJSON");
-                        $_getJSON.withArgs(sinon.match((actualUrl) => {
-                            return [
-                                "https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?",
-                                "callback=?"
-                            ].every((expectedPart) => {
-                                return actualUrl.indexOf(expectedPart) !== -1;
-                            });
-                        })).yields(a_valid_response);
+                [
+                    {
+                        'testName': 'should, with request for 1 valid streamer id, get response from TwitchTV API',
+                        'mock_getJSON_response': a_valid_response_obj,
+                        'streamer_id': "freecodecamp",
+                    },
+                ].forEach((params) => {
+                    it(params.testName, sinon.test(function (done) {
+                        setupJsdomAndAssertWith((err, window, $) => {
+                            //    Given
+                            let $_getJSON = this.stub($, "getJSON");
+                            $_getJSON.withArgs(sinon.match((actualUrl) => {
+                                return [
+                                    format("https://wind-bow.gomix.me/twitch-api/streams/{}\?", params.streamer_id),
+                                    "callback=?"
+                                ].every((expectedPart) => {
+                                    return actualUrl.indexOf(expectedPart) !== -1;
+                                });
+                            })).yields(params.mock_getJSON_response);
 
-                        SystemJS.import('./logic.jsx').then(function (logic) {
-                            //    When
-                            logic.getJsonFromTwitchTV((data) => {
-                                //    Then
-                                expect(data).to.equal(a_valid_response);
-                                done();
+                            SystemJS.import('./logic.jsx').then(function (logic) {
+                                //    When
+                                logic.getJsonFromTwitchTV((data) => {
+                                    //    Then
+                                    expect(data).to.equal(params.mock_getJSON_response);
+                                    done();
+                                });
+                            }).catch(function (err) {
+                                console.log(err); // wont be called either
                             });
-                        }).catch(function (err) {
-                            console.log(err); // wont be called either
                         });
-                    });
-                }));
+                    }));
+                });
             });
         });
 
