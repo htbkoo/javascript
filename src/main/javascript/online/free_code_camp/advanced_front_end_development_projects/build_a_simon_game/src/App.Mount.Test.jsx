d0486935b5d77bb7c1242c4916ab6d959e08f4a9
H.T. Koo <h1017760@connect.hku.hk>
2017-04-30 23:26:35 +0800

    FreeCodeCamp - WIP 1 (Build a Simon Game): refactored App.Mount.Test.jsx to load index.html and fetch external scripts dynamically

--- src/main/javascript/online/free_code_camp/advanced_front_end_development_projects/build_a_simon_game/src/App.Mount.Test.jsx
+++ src/main/javascript/online/free_code_camp/advanced_front_end_development_projects/build_a_simon_game/src/App.Mount.Test.jsx
@@ -10,33 +10,12 @@ sinon.testCase = sinonTest.configureTestCase(sinon);
 import App from './App';
 import Game from './game';
 
-before(function () {
-    jsdom.env('', [
-        require.resolve("../public/jquery-1.11.3/jquery-1.11.3.min.js"),
-        require.resolve("../public/bootstrap-switch-3.3.4/bootstrap-switch-master/js/bootstrap-switch.js")
-    ], function (err, window) {
-        // global.window = document.defaultView;
-        global.window = window;
-        global.document = window.document;
-        Object.keys(document.defaultView).forEach((property) => {
-            if (typeof global[property] === 'undefined') {
-                global[property] = document.defaultView[property];
-            }
-        });
-
-        global.navigator = {
-            userAgent: 'node.js'
-        };
-    });
-});
-
-
 describe("SimonGame (Full render test) - FreeCodeCamp", function () {
     "use strict";
     describe("FrontEnd - Advanced Project", function () {
         describe("SimonGame", function () {
             describe("<App/>", function () {
-                it('shallow renders App without crashing', sinon.test(function () {
+                it('shallow renders App without crashing', sinon.test(function (done) {
                     // Given
                     this.stub(Game.prototype, "getStatus").callsFake(() => {
                         return {
@@ -47,11 +26,40 @@ describe("SimonGame (Full render test) - FreeCodeCamp", function () {
                     });
 
                     // When
-                    mount(<App/>);
-
                     // Then
+                    setUpJsdomAndAssert(() => {
+                        mount(<App/>);
+                    }, done)
                 }));
             });
         });
     });
+
+    function setUpJsdomAndAssert(assertion, done) {
+        jsdom.env({
+            file: require.resolve("../public/index.html"),
+            features: {
+                FetchExternalResources: ["script"],
+                ProcessExternalResources: ["script"],
+                SkipExternalResources: false
+            },
+            created: function (err, window) {
+                global.window = window;
+                global.document = window.document;
+                Object.keys(document.defaultView).forEach((property) => {
+                    if (typeof global[property] === 'undefined') {
+                        global[property] = document.defaultView[property];
+                    }
+                });
+
+                global.navigator = {
+                    userAgent: 'node.js'
+                };
+            },
+            done: (err, window) => {
+                assertion(err, window);
+                done();
+            }
+        });
+    }
 });
