da98b354ba3cb9c1a0b2ecad565a000d308822f9
H.T. Koo <h1017760@connect.hku.hk>
2017-05-07 11:38:16 +0800

    FreeCodeCamp - WIP 1 (Build a Simon Game): minor refactoring of tests structure

--- src/main/javascript/online/free_code_camp/advanced_front_end_development_projects/build_a_simon_game/src/App.Test.jsx
+++ src/main/javascript/online/free_code_camp/advanced_front_end_development_projects/build_a_simon_game/src/App.Test.jsx
@@ -75,144 +75,145 @@ describe("SimonGame - FreeCodeCamp", function () {
                     // Then
                     chai.expect(wrapperDashboard.props.score).to.equal("someScore");
                 }));
-            });
 
-            describe("<Container/>", function () {
-                it("should be a container which has a div.Container which hold the props.children", function () {
-                    //    Given
-                    let aChild = shallow(<img/>);
+                describe("<Container/>", function () {
+                    it("should be a container which has a div.Container which hold the props.children", function () {
+                        //    Given
+                        let aChild = shallow(<img/>);
 
-                    //    When
-                    const wrapperContainer = shallow(<Container>{aChild}</Container>);
-                    const divUnderContainer = wrapperContainer.find('div').get(0);
+                        //    When
+                        const wrapperContainer = shallow(<Container>{aChild}</Container>);
+                        const divUnderContainer = wrapperContainer.find('div').get(0);
 
-                    //    Then
-                    chai.expect(divUnderContainer.props.children).to.equal(aChild);
-                });
-            });
-
-            describe("<Dashboard/>", function () {
-                it("should contain <Score/>, <StrictSwitch/>, <StartButton/>", function () {
-                    //    Given
-                    const wrapperDashboard = shallow(<Dashboard/>);
-                    //    When
-                    //    Then
-                    const containers = wrapperDashboard.find('Container');
-                    chai.expect(containers).to.have.length(3);
-
-                    [
-                        [<Score/>, "<Score/>"],
-                        [<StrictSwitch/>, "<StrictSwitch/>"],
-                        [<StartButton/>, "<StartButton/>"]
-                    ].forEach((element, i) => {
-                        let container = shallow(containers.get(i));
-                        chai.expect(container.containsMatchingElement(element[0]))
-                            .to.equal(true, format("<Dashboard/> should contain {}", element[1]));
+                        //    Then
+                        chai.expect(divUnderContainer.props.children).to.equal(aChild);
                     });
                 });
 
-                it("should pass handleRestart callback to onClick at <StartButton/>", function () {
-                    //    Given
-                    let mockHandleRestartTriggered = false;
-                    const mockHandleRestart = () => {
-                        mockHandleRestartTriggered = true
-                    };
+                describe("<Title/>", function () {
+                    it("should contain <div>{title}</div>", function () {
+                        //    Given
+                        const wrapperTitle = shallow(<Title/>);
 
-                    const wrapperDashboard = shallow(<Dashboard onRestartClicked={mockHandleRestart}/>);
-                    const wrapperStartButton = wrapperDashboard.find("StartButton").get(0);
+                        //    When
+                        const divTitle = wrapperTitle.find("div").get(0);
 
-                    //    When
-                    wrapperStartButton.props.onClick();
-
-                    //    Then
-                    chai.expect(mockHandleRestartTriggered).to.be.true;
+                        //    Then
+                        chai.expect(divTitle.props.children).to.equal("Simon® Game")
+                    });
                 });
 
-                it("should pass score to score at <Score/>", function () {
-                    //    Given
-                    //    When
-                    const wrapperDashboard = shallow(<Dashboard score="someScore"/>);
-                    const wrapperStartButton = wrapperDashboard.find("Score").get(0);
+                describe("<Dashboard/>", function () {
+                    it("should contain <Score/>, <StrictSwitch/>, <StartButton/>", function () {
+                        //    Given
+                        const wrapperDashboard = shallow(<Dashboard/>);
+                        //    When
+                        //    Then
+                        const containers = wrapperDashboard.find('Container');
+                        chai.expect(containers).to.have.length(3);
 
-                    //    Then
-                    chai.expect(wrapperStartButton.props.score).to.equal("someScore");
+                        [
+                            [<Score/>, "<Score/>"],
+                            [<StrictSwitch/>, "<StrictSwitch/>"],
+                            [<StartButton/>, "<StartButton/>"]
+                        ].forEach((element, i) => {
+                            let container = shallow(containers.get(i));
+                            chai.expect(container.containsMatchingElement(element[0]))
+                                .to.equal(true, format("<Dashboard/> should contain {}", element[1]));
+                        });
+                    });
+
+                    it("should pass handleRestart callback to onClick at <StartButton/>", function () {
+                        //    Given
+                        let mockHandleRestartTriggered = false;
+                        const mockHandleRestart = () => {
+                            mockHandleRestartTriggered = true
+                        };
+
+                        const wrapperDashboard = shallow(<Dashboard onRestartClicked={mockHandleRestart}/>);
+                        const wrapperStartButton = wrapperDashboard.find("StartButton").get(0);
+
+                        //    When
+                        wrapperStartButton.props.onClick();
+
+                        //    Then
+                        chai.expect(mockHandleRestartTriggered).to.be.true;
+                    });
+
+                    it("should pass score to score at <Score/>", function () {
+                        //    Given
+                        //    When
+                        const wrapperDashboard = shallow(<Dashboard score="someScore"/>);
+                        const wrapperStartButton = wrapperDashboard.find("Score").get(0);
+
+                        //    Then
+                        chai.expect(wrapperStartButton.props.score).to.equal("someScore");
+                    });
+
+
+                    describe("<Score/>", function () {
+                        it("should display this.props.score", sinon.test(function () {
+                            //    Given
+                            //    When
+                            const wrapperScore = shallow(<Score score="someScore"/>);
+                            const divScore = wrapperScore.find("div").get(0);
+
+                            //    Then
+                            chai.expect(divScore.props.children).to.equal("someScore")
+                        }));
+                    });
+
+                    describe("<StrictSwitch/>", function () {
+                        it("should have a checkbox (unselected by default)", function () {
+                            //    Given
+                            //    When
+                            const wrapperStrictSwitch = shallow(<StrictSwitch/>);
+                            const inputCheckbox = wrapperStrictSwitch.find("input").get(0);
+
+                            //    Then
+                            chai.expect("checked" in inputCheckbox.props).to.be.false;
+                            chai.expect(inputCheckbox.props.type).to.equal("checkbox");
+                        });
+
+                        it("should call game.toggleStrict() when clicked checkbox", sinon.test(function () {
+                            //    Given
+                            const wrapperStrictSwitch = shallow(<StrictSwitch/>);
+                            const inputCheckbox = shallow(wrapperStrictSwitch.find("input").get(0));
+                            const mockGame = this.mock(Game.prototype);
+                            mockGame.expects("toggleStrict").once().returns("");
+
+                            //    When
+                            inputCheckbox.simulate('click');
+
+                            //    Then
+                            mockGame.verify();
+                        }));
+                    });
+
+                    describe("<StartButton/>", function () {
+                        it("should have a button for restarting, i.e. calling game.restart() when clicked and cause state update", sinon.test(function () {
+                            //    Given
+                            const mockGame = this.mock(Game.prototype);
+                            mockGame.expects("restart").once().returns("");
+
+                            let mockClickCallbackTriggered = false;
+                            const mockClickCallback = () => {
+                                // ensure game.restart() is triggered first
+                                mockGame.verify();
+                                mockClickCallbackTriggered = true;
+                            };
+
+                            const wrapperStartButton = shallow(<StartButton onClick={mockClickCallback}/>);
+                            const buttonRestart = shallow(wrapperStartButton.find("button").get(0));
+
+                            //    When
+                            buttonRestart.simulate('click');
+
+                            //    Then
+                            chai.expect(mockClickCallbackTriggered).to.be.true;
+                        }));
+                    });
                 });
-            });
-
-            describe("<Title/>", function () {
-                it("should contain <div>{title}</div>", function () {
-                    //    Given
-                    const wrapperTitle = shallow(<Title/>);
-
-                    //    When
-                    const divTitle = wrapperTitle.find("div").get(0);
-
-                    //    Then
-                    chai.expect(divTitle.props.children).to.equal("Simon® Game")
-                });
-            });
-
-            describe("<Score/>", function () {
-                it("should display this.props.score", sinon.test(function () {
-                    //    Given
-                    //    When
-                    const wrapperScore = shallow(<Score score="someScore"/>);
-                    const divScore = wrapperScore.find("div").get(0);
-
-                    //    Then
-                    chai.expect(divScore.props.children).to.equal("someScore")
-                }));
-            });
-
-            describe("<StrictSwitch/>", function () {
-                it("should have a checkbox (unselected by default)", function () {
-                    //    Given
-                    //    When
-                    const wrapperStrictSwitch = shallow(<StrictSwitch/>);
-                    const inputCheckbox = wrapperStrictSwitch.find("input").get(0);
-
-                    //    Then
-                    chai.expect("checked" in inputCheckbox.props).to.be.false;
-                    chai.expect(inputCheckbox.props.type).to.equal("checkbox");
-                });
-
-                it("should call game.toggleStrict() when clicked checkbox", sinon.test(function () {
-                    //    Given
-                    const wrapperStrictSwitch = shallow(<StrictSwitch/>);
-                    const inputCheckbox = shallow(wrapperStrictSwitch.find("input").get(0));
-                    const mockGame = this.mock(Game.prototype);
-                    mockGame.expects("toggleStrict").once().returns("");
-
-                    //    When
-                    inputCheckbox.simulate('click');
-
-                    //    Then
-                    mockGame.verify();
-                }));
-            });
-
-            describe("<StartButton/>", function () {
-                it("should have a button for restarting, i.e. calling game.restart() when clicked and cause state update", sinon.test(function () {
-                    //    Given
-                    const mockGame = this.mock(Game.prototype);
-                    mockGame.expects("restart").once().returns("");
-
-                    let mockClickCallbackTriggered = false;
-                    const mockClickCallback = () => {
-                        // ensure game.restart() is triggered first
-                        mockGame.verify();
-                        mockClickCallbackTriggered = true;
-                    };
-
-                    const wrapperStartButton = shallow(<StartButton onClick={mockClickCallback}/>);
-                    const buttonRestart = shallow(wrapperStartButton.find("button").get(0));
-
-                    //    When
-                    buttonRestart.simulate('click');
-
-                    //    Then
-                    chai.expect(mockClickCallbackTriggered).to.be.true;
-                }));
             });
         });
     });
