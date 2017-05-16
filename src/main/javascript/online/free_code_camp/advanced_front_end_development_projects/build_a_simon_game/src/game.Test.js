cde51fdc56e58a79c836e81c6cf83711579d1363
H.T. Koo <h1017760@connect.hku.hk>
2017-05-16 08:44:51 +0800

    FreeCodeCamp - WIP 1 (Build a Simon Game): remove test cases leftover in game.Test.js

--- src/main/javascript/online/free_code_camp/advanced_front_end_development_projects/build_a_simon_game/src/game.Test.js
+++ src/main/javascript/online/free_code_camp/advanced_front_end_development_projects/build_a_simon_game/src/game.Test.js
@@ -49,37 +49,6 @@ let testCases = {
             "expectedTrueStatusName": "isPlaying",
             "errorMessage": "Status should be 'playing' when demoed"
         }
-    ],
-    "scoreFormatter": [
-        {
-            "testName": "should format score as '--' if game is not started yet",
-            "isIdle": true,
-            "expectedScore": "--"
-        },
-        {
-            "testName": "should format 0 raw score as '01'",
-            "isIdle": false,
-            "score": 0,
-            "expectedScore": "01"
-        },
-        {
-            "testName": "should format 1 raw score as '02'",
-            "isIdle": false,
-            "score": 1,
-            "expectedScore": "02"
-        },
-        {
-            "testName": "should format 9 raw score as '10'",
-            "isIdle": false,
-            "score": 9,
-            "expectedScore": "10"
-        },
-        {
-            "testName": "should format 19 raw score as '20'",
-            "isIdle": false,
-            "score": 19,
-            "expectedScore": "20"
-        }
     ]
 };
 
