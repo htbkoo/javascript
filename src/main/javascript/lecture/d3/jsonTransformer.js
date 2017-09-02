ed7cfedfa371aca4ad75981ff4afe756f8066f33
H.T. Koo <h1017760@connect.hku.hk>
2017-09-02 18:06:20 +0800

    able to add children and getCopyOfChildren d3/jsonTransformer

--- src/main/javascript/lecture/d3/jsonTransformer.js
+++ src/main/javascript/lecture/d3/jsonTransformer.js
@@ -1,17 +1,71 @@
-function Node() {
+function isObjectEmpty(obj) {
     "use strict";
-    this.asJSON = function(){
+    return Object.keys(obj).length === 0;
+}
+
+function Node(name) {
+    "use strict";
+    // var children = [];
+    var children = {};
+    var primitiveChildren = {};
+
+    this.childrenAsJson = function () {
+        var copyOfChildren = Object.keys(children).map(function (childKey) {
+            return children[childKey].asJSON();
+        });
+        if (!isObjectEmpty(primitiveChildren)) {
+            copyOfChildren.push(primitiveChildren);
+        }
+        return copyOfChildren
+    };
+
+    this.asJSON = function () {
         return {
-            name: "Root",
-            children: []
+            name: name,
+            children: this.childrenAsJson()
         }
     };
+
+    this.addPrimitiveChild = function (key, value) {
+        primitiveChildren[key] = value;
+    };
+
+    this.addJsonChildWithOrder = function (param) {
+        if (!('order' in param) || !('json' in param)) {
+            throw new Error("order and json should be in param");
+        }
+        var order = param.order;
+        if (!(Array.isArray(order))) {
+            throw new Error("param.order should be an Array");
+        }
+        if (order.length > 0) {
+            var key = order[0];
+            if (!(key in children)) {
+                children[key] = new Node(param.json[key]);
+            }
+            children[key].addJsonChildWithOrder({
+                order: order.slice(1),
+                json: param.json
+            });
+        }
+    };
+
     return this;
 }
 
-Node.createNode = function () {
+Node.createNode = function (optionalParam) {
     "use strict";
-    return new Node();
+    return (typeof optionalParam === "undefined") ? new Node("Root") : nodeWithParam(optionalParam);
 };
 
 module.exports = Node;
+
+function nodeWithParam(param) {
+    "use strict";
+    var name = "name" in param ? name : "Root";
+    var node = new Node(name);
+    if ('child' in param) {
+
+    }
+    return node;
+}
--- src/testMocha/javascript/lecture/d3/jsonTransformer.test.js
+++ src/testMocha/javascript/lecture/d3/jsonTransformer.test.js
@@ -1,21 +1,121 @@
 var chai = require("chai");
 
 var srcDirRequire = require.main.require('src/testMocha/testInfrastructure');
-var jsonTransformer = srcDirRequire(__dirname, "jsonTransformer");
+var Node = srcDirRequire(__dirname, "jsonTransformer");
 
-var createNode = jsonTransformer.createNode;
+var createNode = Node.createNode;
 
 describe("jsonTransformer", function () {
     "use strict";
-    it("should transform json into node", function () {
-        //    given
-        //    when
-        var node = createNode();
+    describe("Node", function () {
+        it("should be able to crete node and get asJSON", function () {
+            //    given
+            //    when
+            var node = new Node("");
 
-        //    then
-        chai.expect(node.asJSON()).to.deep.equal({
-            name: "Root",
-            children: []
-        })
+            //    then
+            chai.expect(node.asJSON()).to.deep.equal({
+                name: "",
+                children: []
+            })
+        });
+
+        it("should be able to get copy of children", function () {
+            //    given
+            //    when
+            var node = new Node("");
+            var children = node.childrenAsJson();
+            chai.expect(children).to.be.an('array').that.is.empty;
+            children.push(1);
+
+            //    then
+            chai.expect(node.childrenAsJson()).to.be.an('array').that.is.empty;
+        });
+
+        it("should be able to create node with 1 primitive child and get asJSON", function () {
+            //    given
+            //    when
+            var node = new Node("");
+            node.addPrimitiveChild("size", 200);
+
+            //    then
+            chai.expect(node.asJSON()).to.deep.equal({
+                name: "",
+                children: [{
+                    size: 200
+                }]
+            })
+        });
+
+        it("should be able to crete node and addJsonChildWithOrder ", function () {
+            //    given
+            //    when
+            var node = new Node("Root");
+            node.addJsonChildWithOrder({
+                order: ["Asset Class", "Subasset Class", "Title", "Publication Id"],
+                json: {
+                    "Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pub id"
+                }
+            });
+
+            //    then
+            chai.expect(node.asJSON()).to.deep.equal({
+                name: "Root",
+                children: [{
+                    name: "asset",
+                    children: [{
+                        name: "subAsset",
+                        children: [{
+                            name: "title",
+                            children: [{
+                                name: "pub id",
+                                children: []
+                            }]
+                        }]
+                    }]
+                }]
+            })
+        });
+    });
+
+    xdescribe("jsonTransformer", function () {
+        it("should create node and get as JSON", function () {
+            //    given
+            //    when
+            var node = createNode();
+
+            //    then
+            chai.expect(node.asJSON()).to.deep.equal({
+                name: "Root",
+                children: []
+            })
+        });
+
+        it("should crete node with 1 child", function () {
+            //    given
+            //    when
+            var node = createNode({child: {name: "Bond"}});
+
+            //    then
+            chai.expect(node.asJSON()).to.deep.equal({
+                name: "Root",
+                children: [{
+                    name: "Bond",
+                    children: []
+                }]
+            })
+        });
+
+        xit("should transform json into node", function () {
+            //    given
+            //    when
+            var node = createNode();
+
+            //    then
+            chai.expect(node.asJSON()).to.deep.equal({
+                name: "Root",
+                children: []
+            })
+        });
     });
 });
