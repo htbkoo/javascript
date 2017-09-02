14ecdfdd6ca099a541310259e489fd6d25b4d0d9
H.T. Koo <h1017760@connect.hku.hk>
2017-09-02 18:56:05 +0800

    fixed and added test case d3/jsonTransformer

--- src/testMocha/javascript/lecture/d3/jsonTransformer.test.js
+++ src/testMocha/javascript/lecture/d3/jsonTransformer.test.js
@@ -36,7 +36,7 @@ describe("jsonTransformer", function () {
             //    given
             //    when
             var node = new Node("");
-            node.addPrimitiveChildWithPath(200, "size");
+            node.addPrimitiveChildWithPath(200, ["size"]);
 
             //    then
             chai.expect(node.asJSON()).to.deep.equal({
@@ -47,10 +47,42 @@ describe("jsonTransformer", function () {
             })
         });
 
-        it("should be able to crete node and addJsonChildWithOrder ", function () {
+        it("should be able to crete node and addJsonChildWithPath", function () {
             //    given
             //    when
             var node = new Node("Root");
+            node.addJsonChildWithPath(
+                {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pub id"},
+                ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+            );
+
+            //    then
+            chai.expect(node.asJSON()).to.deep.equal({
+                name: "Root",
+                children: [{
+                    name: "asset",
+                    children: [{
+                        name: "subAsset",
+                        children: [{
+                            name: "title",
+                            children: [{
+                                name: "pub id",
+                                children: []
+                            }]
+                        }]
+                    }]
+                }]
+            })
+        });
+    
+        it("should be able to addJsonChildWithPath with same path ", function () {
+            //    given
+            //    when
+            var node = new Node("Root");
+            node.addJsonChildWithPath(
+                {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pub id"},
+                ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+            );
             node.addJsonChildWithPath(
                 {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pub id"},
                 ["Asset Class", "Subasset Class", "Title", "Publication Id"]
