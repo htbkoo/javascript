cff41bdc2dc94b5ea40fffa663a7be89cf199187
H.T. Koo <h1017760@connect.hku.hk>
2017-09-03 12:43:46 +0800

    grouped test case jsonTransformer

--- src/testMocha/javascript/lecture/d3/jsonTransformer.test.js
+++ src/testMocha/javascript/lecture/d3/jsonTransformer.test.js
@@ -8,176 +8,252 @@ var createNode = Node.createNode;
 describe("jsonTransformer", function () {
     "use strict";
     describe("Node", function () {
-        it("should be able to crete node and get asJSON", function () {
-            //    given
-            //    when
-            var node = new Node("");
+        describe("creation", function () {
+            it("should be able to crete node and get asJSON", function () {
+                //    given
+                //    when
+                var node = new Node("");
 
-            //    then
-            chai.expect(node.asJSON()).to.deep.equal({
-                name: "",
-                children: []
-            })
+                //    then
+                chai.expect(node.asJSON()).to.deep.equal({
+                    name: "",
+                    children: []
+                })
+            });
         });
 
-        it("should be able to get copy of children", function () {
-            //    given
-            //    when
-            var node = new Node("");
-            var children = node.childrenAsJson();
-            chai.expect(children).to.be.an('array').that.is.empty;
-            children.push(1);
+        describe("getChildren/asJSON", function () {
+            it("should be able to get copy of children", function () {
+                //    given
+                //    when
+                var node = new Node("");
+                var children = node.childrenAsJson();
+                chai.expect(children).to.be.an('array').that.is.empty;
+                children.push(1);
 
-            //    then
-            chai.expect(node.childrenAsJson()).to.be.an('array').that.is.empty;
+                //    then
+                chai.expect(node.childrenAsJson()).to.be.an('array').that.is.empty;
+            });
         });
 
-        it("should be able to create node with 1 primitive child and get asJSON", function () {
-            //    given
-            //    when
-            var node = new Node("");
-            node.addPrimitiveChildWithPath(200, ["size"]);
+        describe("primitive children", function () {
+            it("should be able to create node with 1 primitive child and get asJSON", function () {
+                //    given
+                //    when
+                var node = new Node("");
+                node.addPrimitiveChildWithPath(200, ["size"]);
 
-            //    then
-            chai.expect(node.asJSON()).to.deep.equal({
-                name: "",
-                children: [{
-                    size: 200
-                }]
-            })
-        });
-
-        it("should be able to crete node and addJsonChildWithPath", function () {
-            //    given
-            //    when
-            var node = new Node("Root");
-            node.addJsonChildWithPath(
-                {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pub id"},
-                ["Asset Class", "Subasset Class", "Title", "Publication Id"]
-            );
-
-            //    then
-            chai.expect(node.asJSON()).to.deep.equal({
-                name: "Root",
-                children: [{
-                    name: "asset",
+                //    then
+                chai.expect(node.asJSON()).to.deep.equal({
+                    name: "",
                     children: [{
-                        name: "subAsset",
+                        size: 200
+                    }]
+                })
+            });
+        });
+
+        describe("node children", function () {
+            it("should be able to crete node and addJsonChildWithPath", function () {
+                //    given
+                //    when
+                var node = new Node("Root");
+                node.addJsonChildWithPath(
+                    {
+                        "Asset Class": "asset",
+                        "Subasset Class": "subAsset",
+                        "Title": "title",
+                        "Publication Id": "pub id"
+                    },
+                    ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+                );
+
+                //    then
+                chai.expect(node.asJSON()).to.deep.equal({
+                    name: "Root",
+                    children: [{
+                        name: "asset",
                         children: [{
-                            name: "title",
+                            name: "subAsset",
                             children: [{
-                                name: "pub id",
-                                children: []
+                                name: "title",
+                                children: [{
+                                    name: "pub id",
+                                    children: []
+                                }]
                             }]
                         }]
                     }]
-                }]
-            })
-        });
+                })
+            });
 
-        it("should be able to addJsonChildWithPath with same path", function () {
-            //    given
-            //    when
-            var node = new Node("Root");
-            node.addJsonChildWithPath(
-                {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pub id"},
-                ["Asset Class", "Subasset Class", "Title", "Publication Id"]
-            );
-            node.addJsonChildWithPath(
-                {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pub id"},
-                ["Asset Class", "Subasset Class", "Title", "Publication Id"]
-            );
+            it("should be able to addJsonChildWithPath with same path", function () {
+                //    given
+                //    when
+                var node = new Node("Root");
+                node.addJsonChildWithPath(
+                    {
+                        "Asset Class": "asset",
+                        "Subasset Class": "subAsset",
+                        "Title": "title",
+                        "Publication Id": "pub id"
+                    },
+                    ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+                );
+                node.addJsonChildWithPath(
+                    {
+                        "Asset Class": "asset",
+                        "Subasset Class": "subAsset",
+                        "Title": "title",
+                        "Publication Id": "pub id"
+                    },
+                    ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+                );
 
-            //    then
-            chai.expect(node.asJSON()).to.deep.equal({
-                name: "Root",
-                children: [{
-                    name: "asset",
+                //    then
+                chai.expect(node.asJSON()).to.deep.equal({
+                    name: "Root",
                     children: [{
-                        name: "subAsset",
+                        name: "asset",
                         children: [{
-                            name: "title",
+                            name: "subAsset",
                             children: [{
-                                name: "pub id",
-                                children: []
+                                name: "title",
+                                children: [{
+                                    name: "pub id",
+                                    children: []
+                                }]
                             }]
                         }]
                     }]
-                }]
-            })
-        });
+                })
+            });
 
-        it("should be able to addJsonChildWithPath with different path (leaf)", function () {
-            //    given
-            //    when
-            var node = new Node("Root");
-            node.addJsonChildWithPath(
-                {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pubId"},
-                ["Asset Class", "Subasset Class", "Title", "Publication Id"]
-            );
-            node.addJsonChildWithPath(
-                {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pubId2"},
-                ["Asset Class", "Subasset Class", "Title", "Publication Id"]
-            );
+            it("should be able to addJsonChildWithPath with different path (leaf)", function () {
+                //    given
+                //    when
+                var node = new Node("Root");
+                node.addJsonChildWithPath(
+                    {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pubId"},
+                    ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+                );
+                node.addJsonChildWithPath(
+                    {
+                        "Asset Class": "asset",
+                        "Subasset Class": "subAsset",
+                        "Title": "title",
+                        "Publication Id": "pubId2"
+                    },
+                    ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+                );
 
-            //    then
-            chai.expect(node.asJSON()).to.deep.equal({
-                name: "Root",
-                children: [{
-                    name: "asset",
+                //    then
+                chai.expect(node.asJSON()).to.deep.equal({
+                    name: "Root",
                     children: [{
-                        name: "subAsset",
+                        name: "asset",
                         children: [{
-                            name: "title",
+                            name: "subAsset",
                             children: [{
-                                name: "pubId",
-                                children: []
+                                name: "title",
+                                children: [{
+                                    name: "pubId",
+                                    children: []
+                                }, {
+                                    name: "pubId2",
+                                    children: []
+                                }]
+                            }]
+                        }]
+                    }]
+                })
+            });
+
+            it("should be able to addJsonChildWithPath with different path (non-leaf)", function () {
+                //    given
+                //    when
+                var node = new Node("Root");
+                node.addJsonChildWithPath(
+                    {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pubId"},
+                    ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+                );
+                node.addJsonChildWithPath(
+                    {
+                        "Asset Class": "asset",
+                        "Subasset Class": "subAsset",
+                        "Title": "title2",
+                        "Publication Id": "pubId"
+                    },
+                    ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+                );
+
+                //    then
+                chai.expect(node.asJSON()).to.deep.equal({
+                    name: "Root",
+                    children: [{
+                        name: "asset",
+                        children: [{
+                            name: "subAsset",
+                            children: [{
+                                name: "title",
+                                children: [{
+                                    name: "pubId",
+                                    children: []
+                                }]
                             }, {
-                                name: "pubId2",
-                                children: []
+                                name: "title2",
+                                children: [{
+                                    name: "pubId",
+                                    children: []
+                                }]
                             }]
                         }]
                     }]
-                }]
-            })
-        });
+                })
+            });
 
-        it("should be able to addJsonChildWithPath with different path (non-leaf)", function () {
-            //    given
-            //    when
-            var node = new Node("Root");
-            node.addJsonChildWithPath(
-                {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pubId"},
-                ["Asset Class", "Subasset Class", "Title", "Publication Id"]
-            );
-            node.addJsonChildWithPath(
-                {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title2", "Publication Id": "pubId"},
-                ["Asset Class", "Subasset Class", "Title", "Publication Id"]
-            );
+            it("should be able to addJsonChildWithPath with different path (non-leaf)", function () {
+                //    given
+                //    when
+                var node = new Node("Root");
+                node.addJsonChildWithPath(
+                    {"Asset Class": "asset", "Subasset Class": "subAsset", "Title": "title", "Publication Id": "pubId"},
+                    ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+                );
+                node.addJsonChildWithPath(
+                    {
+                        "Asset Class": "asset",
+                        "Subasset Class": "subAsset",
+                        "Title": "title2",
+                        "Publication Id": "pubId"
+                    },
+                    ["Asset Class", "Subasset Class", "Title", "Publication Id"]
+                );
 
-            //    then
-            chai.expect(node.asJSON()).to.deep.equal({
-                name: "Root",
-                children: [{
-                    name: "asset",
+                //    then
+                chai.expect(node.asJSON()).to.deep.equal({
+                    name: "Root",
                     children: [{
-                        name: "subAsset",
+                        name: "asset",
                         children: [{
-                            name: "title",
+                            name: "subAsset",
                             children: [{
-                                name: "pubId",
-                                children: []
-                            }]
-                        }, {
-                            name: "title2",
-                            children: [{
-                                name: "pubId",
-                                children: []
+                                name: "title",
+                                children: [{
+                                    name: "pubId",
+                                    children: []
+                                }]
+                            }, {
+                                name: "title2",
+                                children: [{
+                                    name: "pubId",
+                                    children: []
+                                }]
                             }]
                         }]
                     }]
-                }]
-            })
+                })
+            });
         });
     });
 });
